93417c97efc33c438821d521b59bb8ae23b6da6f
Jannik Mueller <jannik@andreasmueller.de>
2026-05-11 01:15:57 +0200

    Hybris: Modify to make kernel build
    Change-Id: I9d6031b847483b37986151aaef9dc3874373afd8

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -9,3 +9,39 @@ BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modu
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_kernel_boot))
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+TARGET_KERNEL_CONFIG := pantah_defconfig
+#$(info TARGET_KERNEL_DIR=$(TARGET_KERNEL_DIR))
+#$(info BOARD_PREBUILT_BOOTIMAGE=$(BOARD_PREBUILT_BOOTIMAGE))
+TARGET_KERNEL_SOURCE := kernel/google/pantah/panther
+TARGET_KERNEL_EXT_MODULE_ROOT := private/google-modules
+TARGET_KERNEL_EXT_MODULES := \
+    amplifiers/audiometrics \
+    amplifiers/cs35l41 \
+    amplifiers/cs35l45 \
+    amplifiers/cs40l26 \
+    amplifiers/snd_soc_wm_adsp \
+    aoc \
+    bluetooth/broadcom \
+    bluetooth/qcom \
+    bms \
+    display/samsung \
+    edgetpu/janeiro \
+    fingerprint/fpc \
+    gpu/mali_kbase \
+    gpu/mali_pixel \
+    gxp/gs201 \
+    hdcp/samsung \
+    lwis \
+    misc/sscoredump \
+    nfc \
+    power/mitigation \
+    power/reset \
+    sensors/hall_sensor \
+    soc/gs \
+    touch/common \
+    touch/goodix \
+    touch/sec \
+    trusty \
+    video/gchips \
+    wlan/bcm4389 \
+    wlan/wlan_ptracker
